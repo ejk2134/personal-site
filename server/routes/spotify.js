078da16b8c6ddf8c8bc015597830014b18c3039c
Evan Kearney <evanjkearney@gmail.com>
2018-01-04 15:12:16 -0600

    Receive my top tracks from spotify api -- but won't work if refresh token becomes invalid at any point, which is tbd

--- server/routes/spotify.js
+++ server/routes/spotify.js
@@ -7,43 +7,51 @@ const SpotifyWebAPI = require('spotify-web-api-node')
 
 router.get('/', (req, res)=>{
     console.log('Spotify request');
-
+    //spotify client credentials
     let auth = 'Basic ' + new Buffer(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64');
 
+    // options for access token request
     let options = {
         url: 'https://accounts.spotify.com/api/token',
         method: 'POST',
         form: {
-            grant_type: 'client_credentials'
+            grant_type: 'refresh_token',
+            refresh_token: process.env.SPOTIFY_AUTH_CODE
         },
         headers: {
             authorization: auth
         },
         json: true
     }
-
+    //access token request
     rp(options)
         .then((parsedBody) => {
-            res.send(parsedBody);
+            console.log('.then');
+            console.log(parsedBody);
+            let accessToken = parsedBody.access_token;
+
+            // options for my top tracks
+            let options = {
+                url: 'https://api.spotify.com/v1/me/top/tracks?time_range=short_term',
+                method: 'GET',
+                headers: {
+                    authorization: 'Bearer ' + accessToken
+                },
+                json: true
+            }
+
+            //top tracks request
+            request(options, (err, resp, body)=>{
+                console.log('error:', err);
+                console.log('status code:', resp && resp.statusCode);
+                console.log('body:', body);
+                res.send(body);
+            })
         })
         .catch((err) => {
+            console.log('.catch');
             res.send(err);
         })
-
-    // request(options, (err, resp, body)=>{
-    //     console.log('error:', err);
-    //     console.log('status code:', resp && resp.statusCode);
-    //     console.log('body:', body);
-    //     res.status(200).send(JSON.parse(body));
-    // })
-
-    // let options = {
-    //     url: 'https://api.spotify.com/v1/me/top/tracks',
-    //     method: 'GET',
-    //     headers: {
-    //         authorization: 'Bearer 3c8d331c0d0d413a9135a4c700702a9e'
-    //     }
-    // }
 })
 
 module.exports = router;
